dd8b2f523d0cc58a1ce4c26dbdb0b33fc7beca34
Pigiotyreal <pigiotyreal@gmail.com>
2023-08-30 19:55:54 -0400

    it works.. ish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
 import requests
 import time
-import asyncio
+import threading
 
 print("Welcome to Crypto Investment Simulator! Type help to get help!")
 print("You have $1000 to invest. Good luck!")
@@ -12,20 +11,34 @@ eth = 0
 doge = 0
 ltc = 0
 
-async def getPrice():
+btc_price = 0
+eth_price = 0
+doge_price = 0
+ltc_price = 0
+
+def getPrice():
+    global btc_price, eth_price, doge_price, ltc_price
     btc_price = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd").json()["bitcoin"]["usd"]
     eth_price = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd").json()["ethereum"]["usd"]
     doge_price = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd").json()["dogecoin"]["usd"]
     ltc_price = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd").json()["litecoin"]["usd"]
-    return btc_price, eth_price, doge_price, ltc_price
 
-async def main():
+def updatePrices():
     while True:
-        btc_price, eth_price, doge_price, ltc_price = await getPrice()
-        print(f"BTC Price: {btc_price}")
-        print(f"ETH Price: {eth_price}")
-        print(f"DOGE Price: {doge_price}")
-        print(f"LTC Price: {ltc_price}")
-        await asyncio.sleep(20)
+        getPrice()
+        time.sleep(20)
 
-asyncio.run(main())
+threading.Thread(target=updatePrices).start()
+
+while True:
+    command = input("> ")
+    command = command.lower()
+    
+    if command == "help":
+        print("Commands: help, buy, sell, wallet, price, quit")
+    
+    elif command == "price":
+        print("Bitcoin (BTC) price: $", btc_price)
+        print("Ethereum (ETH) price: $", eth_price)
+        print("Dogecoin (DOGE) price: $", doge_price)
+        print("Litecoin (LTC) price: $", ltc_price)
